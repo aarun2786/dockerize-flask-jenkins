2346d5a39207b47fa6b7a57eb36dd23168db7993
root <root@ip-172-31-10-175.ap-south-2.compute.internal>
2026-05-21 18:55:52 +0000

    Added new route the app.py

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def home():
     return "Hello, World! Your Flask app is running successfully."
 
+@app.route("/profile")
+def profile('/profile')
+    return "Thsi is from profile path"
+
 # Run the application if this file is executed directly
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
